384f8f8f704dc827f5f2c4930a539acce3d6f353
SaadNabeel <saadnabeel2010@gmail.com>
2024-10-05 20:17:13 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+
+def information():
+    print("Learning Python")
+    print("Using Replit platform")
+
+
+information()
+information()
+information()
+information()
+information()
+
+
+def add(num1, num2):
+    return num1 + num2
+
+
+print(add(3, 5))
